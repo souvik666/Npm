139eac81b320344140cda5ffa5fc510367216ae3
souvik666 <peacedynasty0@gmail.com>
2021-08-12 23:19:23 +0530

    cli qrcode

---
+++ index.js
@@ -0,0 +1,31 @@
+const { Command } = require("commander");
+var QRCode = require("qrcode");
+/* for example from the CLI I should be able to write in this format:
+node index.js <text_for_qrcode> --output filename.png */
+/* QRCode.toString(filename, { type: "terminal" }, function (err, url) {
+  console.log(url);
+}); */
+let fname;
+const program = new Command();
+program.version("0.0.1");
+program
+  .argument("<nameforqr>")
+  .argument("<filename>")
+  .action(function (nameforqr, filename) {
+    //console.log(filename, nameforqr);
+    QRCode.toFile(
+      `/home/souvik/cake/NPM/${filename}.jpg`,
+      `${nameforqr}`,
+      {
+        color: {
+          dark: "#00F", // Blue dots
+          light: "#0000", // Transparent background
+        },
+      },
+      function (err) {
+        if (err) throw err;
+        console.log("done");
+      }
+    );
+  });
+program.parse();
